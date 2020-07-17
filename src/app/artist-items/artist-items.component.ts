67cdcc06910c649f5fcd2b2e701c2962e8d4c6e2
andresfonky <andresfonquernie@gmail.com>
2020-07-17 13:37:23 +0200

    Learning Angular

---
+++ src/app/artist-items/artist-items.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-items',
+  templateUrl: './artist-items.component.html'
+})
+
+export class ArtistItemsComponent implements OnInit {
+
+  @Input() artist: any = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
